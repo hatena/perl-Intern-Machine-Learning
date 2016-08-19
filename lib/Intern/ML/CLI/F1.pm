db4e46569b6b5870562e4c16395234843f67270a
tarao <tarao.gnn@gmail.com>
2016-08-19 15:11:31 +0900

    Simplify not to depend on feature values.

--- lib/Intern/ML/CLI/F1.pm
+++ lib/Intern/ML/CLI/F1.pm
@@ -11,26 +11,29 @@ sub run {
     my $eval = Intern::ML::Evaluation::F1->new;
 
     my $dh = \*DATA;
-    $class->with_data_set($dh, 'Foo', sub {
-        my ($data) = @_;
-        my $predicted = $data->{features}->[0] == 1
-            ? Intern::ML::Model::Label::Positive
-            : Intern::ML::Model::Label::Negative;
-        my $supervised = $data->{label};
+    for my $line (<$dh>) {
+        chomp $line;
+        last unless $line;
+
+        my ($predicted, $supervised) = map {
+            $_ eq 'Positive'
+                ? Intern::ML::Model::Label::Positive
+                : Intern::ML::Model::Label::Negative;
+        } split ',', $line;
         $eval->add($predicted, $supervised);
-    });
+    }
 
     printf "%f\n", $eval->accuracy;
 }
 
 1;
 __DATA__
-1,2,3,4,Foo
-5,6,7,8,Foo
-1,3,5,7,Foo
-2,4,6,8,Foo
-1,1,2,3,Bar
-1,4,1,4,Foo
-1,7,3,2,Bar
-3,1,4,1,Bar
-2,7,1,8,Foo
+Positive,Positive
+Negative,Positive
+Positive,Positive
+Negative,Positive
+Positive,Negative
+Positive,Positive
+Positive,Negative
+Negative,Negative
+Negative,Positive
